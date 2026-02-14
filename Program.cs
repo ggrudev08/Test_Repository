f26679d58f2a0bc16422cd1ada728bc88a9046dc
Georgi Grudev <ggrudev08@gmail.com>
2026-02-14 11:14:26 +0200

    First comm

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Tomfoolery
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                Dictionary<int, int> map = new Dictionary<int, int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int difference = target - nums[i];
+                    if (!map.ContainsKey(nums[i]))
+                    {
+                        map[difference] = i;
+                    }
+                    else
+                    {
+                        return [map[nums[i]], i];
+                    }
+
+                }
+                return [];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(string.Join(' ', solution.TwoSum([3, 4, 5, 6], 7)));
+        }
+    }
+}
---
+++ Tomfoolery/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Tomfoolery
+{
+    public class Globals
+    {
+        public static int state = 0;
+    }
+
+   public class ConsolePrinter
+    {
+        public static void PrintSomething()
+        {
+            if (Globals.state == 0)
+            {
+                Console.WriteLine("Hello");
+            }
+            else
+            {
+                Console.WriteLine("Bye");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ConsolePrinter.PrintSomething();
+        }
+    }
+}
